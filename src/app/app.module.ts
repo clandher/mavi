23845b1fb75eb7c562ba42c7f6bc8452467494e8
Armando Rodriguez <clandher@gmail.com>
2025-10-17 01:27:04 -0600

    adjust base href

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ModalComponent } from './components/modal/modal.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
   ],
   providers: [],
   bootstrap: []
